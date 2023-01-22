25db3c3c48db01c48f7317a96d4791def950a24a
Robin Sahu <robin.sahu@paytm.com>
2023-01-22 17:19:42 +0530

    added projects

---
+++ learning/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex-container">
+      <div className="item l1">1</div>
+      <div className="item l2">2</div>
+      <div className="item">3</div>
+      <div className="item">4</div>
+      <div className="item">5</div>
+    </div>
+  );
+}
+
+export default App;
